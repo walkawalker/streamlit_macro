bd2c5989f404bba0197bd4a22130bf8aee32718b
walkawalker <74316246+walkawalker@users.noreply.github.com>
2024-09-24 18:30:48 -0400

    changed data code

--- macro_econ_st.py
+++ macro_econ_st.py
@@ -4,6 +4,88 @@ import requests
 import numpy as np
 import streamlit as st 
 import pandas_datareader as pdr
+def create_chloropleth(df, counties_url):
+    counties = alt.topo_feature(counties_url,'counties')
+    selection = alt.selection_point(fields=['Value'])
+    color = alt.condition(selection,
+                alt.Color('Value:Q',
+                scale=alt.Scale(scheme='viridis'),
+                legend=alt.Legend(title='Rate', gradientLength=300)),
+                alt.value('lightgray'))
+    base = alt.Chart(counties).mark_geoshape(stroke='black',
+            strokeWidth=1).encode(
+        color=color,#tooltip="Value:Q"
+        tooltip=[
+            alt.Tooltip("County:O", title="Name"),
+            alt.Tooltip("Lowest_Unemployment:O", title="Lowest UnRate"),
+            alt.Tooltip("Highest_Unemployment:O", title="Highest UnRate"),
+            alt.Tooltip("Lowest_Unemployment_Date:O", title="Lowest UnRate Date"),
+            alt.Tooltip("Highest_Unemployment_Date:O", title="Highest UnRate Date"),
+            alt.Tooltip("Value:O", title="Rate")] #Highest_Unemployment	Lowest_Unemploymen
+    ).transform_lookup(
+        lookup='id',
+        from_=alt.LookupData(df, 'Lookup_id', ['Value', 'County','Highest_Unemployment'	,'Lowest_Unemployment','Highest_Unemployment_Date','Lowest_Unemployment_Date'])
+    ).project(
+        type='albersUsa'
+    ).properties(
+        width=900,
+        height=700
+    ).add_params(selection)
+
+    return base
+def flatten_data_chloro(chloropleth_df):
+    min_max_dates_df = chloropleth_df.groupby(['County', 'State'], as_index=False).agg(
+    Highest_Unemployment=('Value', 'max'),
+    Lowest_Unemployment=('Value', 'min'))
+
+    highest_dates_df = chloropleth_df.loc[chloropleth_df.groupby(['County', 'State'])['Value'].idxmax(), ['County', 'State', 'DATE']]
+    highest_dates_df.rename(columns={'DATE': 'Highest_Unemployment_Date'}, inplace=True)
+
+    lowest_dates_df = chloropleth_df.loc[chloropleth_df.groupby(['County', 'State'])['Value'].idxmin(), ['County', 'State', 'DATE']]
+    lowest_dates_df.rename(columns={'DATE': 'Lowest_Unemployment_Date'}, inplace=True)
+
+    min_max_with_dates_df = pd.merge(min_max_dates_df, highest_dates_df, on=['County', 'State'], how='left')
+    min_max_with_dates_df = pd.merge(min_max_with_dates_df, lowest_dates_df, on=['County', 'State'], how='left')
+
+    filtered_df = chloropleth_df[chloropleth_df['DATE'] == "2024-07-01"]
+
+    return pd.merge(filtered_df, min_max_with_dates_df, on=['County', 'State'], how='left')
+
+def get_county_dataset(file_path):
+    sheet_names = [f"Monthly_{i}" for i in range(1, 14)]
+    merged_df=pd.DataFrame()
+    for sheet in sheet_names:
+        current_df = pd.read_excel(file_path, sheet_name=sheet)
+        
+        if merged_df.empty:
+            merged_df = current_df
+        else:
+            merged_df = merged_df.merge(current_df, on='DATE', how='left')
+    return pd.melt(merged_df, id_vars=['DATE'], var_name='Series_Type', value_name='Value')
+
+def match_state_codes(df_names,state_codes):
+    lookup_ids = []
+
+    for index, row in df_names.iterrows():
+        county = row['County']
+        state = row['State']
+
+        match = state_codes[(state_codes['COUNTYNAME'].str.upper() == county.upper()) & (state_codes['STATE'] == state)]
+        
+        if not match.empty:
+            lookup_id = match['STATEFP'].astype(int).astype(str).values[0] + match['COUNTYFP'].values[0]
+            lookup_ids.append(lookup_id)
+        else:
+            lookup_ids.append('NA')
+
+    df_names['Lookup_id'] = lookup_ids
+    return df_names
+
+def split_description(description):
+    state = description.split(',')[-1].strip()
+    county_or_area = description.split(' in ')[-1].split(',')[0].strip()
+    
+    return county_or_area, state
 
 def get_real_gdp(BEA_file):
     GDP_Q = (BEA_file.parse(sheet_name = "T10101-Q",skiprows=7)
@@ -92,9 +174,9 @@ def create_histogram(df_GDP, option, start_date, end_date, step_size):
     date = ranked_text.encode(text='date:T').properties(title=alt.TitleParams(text='Date', align='right'))
     quarter = ranked_text.encode(text='date_qtr:N').properties(title=alt.TitleParams(text='Quarter', align='right'))
     pchange = ranked_text.encode(text='percent_change:Q').properties(title=alt.TitleParams(text='% Change', align='right'))
-    text = alt.vconcat(date, quarter, pchange) 
+    text = alt.hconcat(date, quarter, pchange) 
 
-    histogram_data_table = alt.vconcat(
+    histogram_data_table = alt.hconcat(
     bars+bars_selection+labels,
         text
     ).resolve_legend(
@@ -105,15 +187,34 @@ def create_histogram(df_GDP, option, start_date, end_date, step_size):
 def get_recession_indicator(df): 
     return pdr.get_data_fred('USREC',start = min(df.date)).reset_index().rename(columns={'DATE' : 'date'})
 
-url = 'https://apps.bea.gov/national/Release/XLS/Survey/Section1All_xls.xlsx'
+GDP_url = 'https://apps.bea.gov/national/Release/XLS/Survey/Section1All_xls.xlsx'
+url_2 = "https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fwalkawalker%2Fstreamlit_macro%2Frefs%2Fheads%2Fmain%2FCounties_UnRate.xls&wdOrigin=BROWSELINK"
+match_keys_url = "https://www2.census.gov/geo/docs/reference/codes2020/national_county2020.txt"
+names_url = "https://raw.githubusercontent.com/walkawalker/streamlit_macro/refs/heads/main/Counties_UnRate_Names.csv"
+missing_url = "https://raw.githubusercontent.com/walkawalker/streamlit_macro/refs/heads/main/Counties_UnRate_Missing_edited.csv"
+counties_url = 'https://raw.githubusercontent.com/walkawalker/streamlit_macro/refs/heads/main/counties.json'
 #BEA_file = pd.ExcelFile(url)
-df_GDP = get_real_gdp(pd.ExcelFile(url))
+df_GDP = get_real_gdp(pd.ExcelFile(GDP_url))
 rec_ind_df = get_recession_indicator(df_GDP)
 merge_GDP_ind_df = df_GDP.merge(rec_ind_df, on='date', how='left')
+
+df_state_codes = pd.read_csv(match_keys_url, delimiter='|')
+df_state_codes['COUNTYFP']=df_state_codes['COUNTYFP'].astype(str).str.zfill(3)
+df_names = pd.read_csv(names_url)
+df_names.drop(columns = ['Unnamed: 0'],inplace = True)
+df_names[['County','State']] = df_names['Series_Name'].apply(split_description).apply(pd.Series)
+df_missing = pd.read_csv(missing_url)
+df_missing.drop(columns = ['Unnamed: 0'],inplace = True)
+
+match_df = match_state_codes(df_names, df_state_codes)
+resultant_df = pd.concat([match_df,df_missing])
+df_county_long = get_county_dataset(url_2)
+chloropleth_df = df_county_long.merge(resultant_df, on='Series_Type', how='left')
+
 with st.container():
     st.altair_chart(create_boxplot(df_GDP), use_container_width=True)
     
-
+'''
 with st.container():
     unique_dates = sorted(merge_GDP_ind_df['date'].unique())
     start_date, end_date = st.select_slider(
@@ -127,4 +228,6 @@ with st.container():
 
     
     st.altair_chart(create_histogram(merge_GDP_ind_df, option, start_date, end_date, 3),key="alt_chart", on_select="rerun", use_container_width=True)
-
+'''
+with st.container():
+    st.altair_chart(create_chloropleth(flatten_data_chloro(chloropleth_df),counties_url), use_container_width=True)
